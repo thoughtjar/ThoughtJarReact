b4269c5a3a17a32fca567e7a54fcd6b7eef34aea
xeliot <daveho8888@gmail.com>
2018-05-03 18:08:55 -0700

    upload to heroku

--- src/containers/Home.js
+++ src/containers/Home.js
@@ -4,6 +4,16 @@ import { Carousel } from "react-bootstrap";
 import carousel from './carousel.png'
 
 export default class Home extends Component {
+
+  constructor(props, context) {
+    super(props, context);
+    this.activateLasers = this.activateLasers.bind(this);
+  }
+
+  activateLasers() {
+    this.props.history.push('/login')
+  }
+
   render() {
     return (
       <div className="Home">
@@ -11,6 +21,9 @@ export default class Home extends Component {
           <h1>Create Your Own Jar.</h1>
           <p>Choose your style.</p>
         </div>
+        <button onClick={this.activateLasers}>
+          Activate Lasers
+        </button>
         <Carousel className="Templates">
           <Carousel.Item>
             <img width={900} height={500} alt="900x500" src={carousel} />
