06d7f4df27a9184a753181c6ee3b16738a116f3a
xeliot <daveho8888@gmail.com>
2018-05-04 21:40:26 -0700

    begin signup route

--- src/Routes.js
+++ src/Routes.js
@@ -3,12 +3,14 @@ import { Route, Switch } from "react-router-dom";
 import Home from "./containers/Home";
 import NotFound from "./containers/NotFound";
 import Login from "./containers/Login";
+import Signup from "./containers/Signup";
 import AboutUs from "./containers/AboutUs";
 
 export default () =>
   <Switch>
     <Route path="/" exact component={Home} />
     <Route path="/login" exact component={Login} />
+    <Route path="/signup" exact component={Signup} />
     <Route path="/aboutus" exact component={AboutUs} />
     <Route component={NotFound} />
   </Switch>;
