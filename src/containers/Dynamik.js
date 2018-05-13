7a78e05854e5d176ae546f9eb60d850b2d446f08
xeliot <daveho8888@gmail.com>
2018-05-12 22:17:17 -0700

    successfully implemented dyanmic adding of shortanswer questions

--- src/containers/Dynamik.js
+++ src/containers/Dynamik.js
@@ -18,17 +18,34 @@ class ShortAnswer extends Component {
 }
 
 export default class Dynamik extends Component {
+  constructor (props) {
+    super(props);
+    this.state = {
+      surveyquestions: []
+    };
+    this.createShortAnswer = this.createShortAnswer.bind(this);
+    this.loadSurveyQuestions = this.loadSurveyQuestions.bind(this);
+  }
+
+  createShortAnswer() {
+    const newSurveyQuestions = this.state.surveyquestions.concat(<ShortAnswer key={this.state.surveyquestions.length + 1} />);
+    this.setState({surveyquestions: newSurveyQuestions});
+  }
+
+  loadSurveyQuestions() {
+    return (this.state.surveyquestions);
+  }
+
   render() {
     return(
       <div className="Dynamik">
         <div className="DynamikHeader">
           <h2>Welcome To Dynamik.</h2>
         </div>
-        <ShortAnswer />
-        <ShortAnswer />
+        {this.loadSurveyQuestions()}
         <ButtonToolbar className="add-question">
-          <DropdownButton bsSize="large" title="ADD" id="dropdown-size-large" dropup pullRight>
-            <MenuItem eventKey="1">Short Answer Question</MenuItem>
+          <DropdownButton bsSize="large" title="Add" id="dropdown-size-large" dropup pullRight>
+            <MenuItem eventKey="1" onClick={this.createShortAnswer}>Short Answer Question</MenuItem>
             <MenuItem eventKey="2">Long Answer Question</MenuItem>
             <MenuItem eventKey="3">Number Question</MenuItem>
             <MenuItem eventKey="4">Multiple Choice Question</MenuItem>
