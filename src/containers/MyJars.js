ca359c1f1466c478bc3c869801803195f4105066
xeliot <daveho8888@gmail.com>
2018-05-17 22:41:32 -0700

    switch to use asynchronous fetch for api calls

--- src/containers/MyJars.js
+++ src/containers/MyJars.js
@@ -5,31 +5,43 @@ export default class MyJars extends Component {
   constructor(props){
     super(props);
     this.state = {
-      value : []
+      requestFailed : false
     }
-    this.keycount = 0;
     this.getJars = this.getJars.bind(this);
   }
 
   getJars(url){
-    var xmlHttp = new XMLHttpRequest();
-    xmlHttp.open("GET", url, false);
-    xmlHttp.send(null);
-    return xmlHttp.responseText;
+    fetch(url)
+      .then(response => {
+        if(!response.ok) {
+          throw Error("Network Request Failed")
+        }
+        return response
+      })
+      .then(d => d.json())
+      .then(d => {
+        this.setState({
+          data: d
+        })
+      }, () => {
+        this.setState({
+          requestFailed: true
+        })
+      })
   }
 
   componentDidMount(){
-    const response = this.getJars("http://35.160.134.174/weather/kansas+city");
-    const newValue = this.state.value.concat(<p key={this.keycount}>{response}</p>);
-    this.keycount += 1;
-    this.setState({value: newValue});
+    this.getJars("https://api.github.com/users/xeliot");
   }
 
   render() {
+
+    if(this.state.requestFailed) return <h4>Failed!</h4>
+    if(!this.state.data) return <h4>Loading ...</h4>
     return(
       <div className="MyJars">
         <h2>My Jar Dashboard</h2>
-        {this.state.value}
+        {this.state.data.name}
       </div>
     );
   }
