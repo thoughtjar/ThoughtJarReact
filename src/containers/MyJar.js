3ed89df363fca67ceae25e2c13d84ea163b329ea
xeliot <daveho8888@gmail.com>
2018-06-11 11:27:23 -0700

    initalize myjar and add csv-export package

---
+++ src/containers/MyJar.js
@@ -0,0 +1,30 @@
+import React, { Component } from "react";
+import {  } from "react-bootstrap";
+import "./MyJar.css";
+import cookie from 'react-cookies';
+
+var queryString = require('queryString');
+var csv_export = require('csv-export');
+
+export default class MyJar extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      title: '',
+      responseContent: {}
+    };
+  }
+
+  render() {
+    return(
+      <div className="MyJar">
+        <div className="Header">
+          <h2>{this.state.title}</h2>
+        </div>
+        <div className="Body">
+          Body Content
+        </div>
+      </div>
+    );
+  }
+}
